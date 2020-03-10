5eb968b8d559555448329149ba176153ae9b4a3c
Anton Stepurko <stepurko.anton@gmail.com>
2020-03-10 17:39:18 +0300

    Реализация многоразового использования элементов.

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+include("includes/header.html");
+
+if ($_SERVER["REQUEST_URI"] == "/authorization") {
+    include("authorization.html");
+} else if ($_SERVER["REQUEST_URI"] == "/registration" || "/") {
+    include("registration.html");
+} 
+
+include("includes/footer.html");
